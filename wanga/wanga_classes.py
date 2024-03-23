45ae9b0fad99535a3ba6470db5c19309533ac66e
Pavel Belyaev <50247580+polo-man@users.noreply.github.com>
2024-03-23 15:26:10 +0300

    Update wanga_classes.py

--- wanga/wanga_classes.py
+++ wanga/wanga_classes.py
@@ -8,7 +8,7 @@ ENDPOINT_FORECAST = "http://IP ВИРТУАЛЬНОЙ МАШИНЫ/f"
 # Класс взаимодействия с BQ
 class workWithBQ(object): 
     name = 'WorkWithBQ'
-    def __init__(self, project_id = 'ID ПРОЕКТА В BQ', creds_folder = CREDS_FOLDER, creds_file = CREDS_FILE, forecast_marker = None, sender = None, comment_text = None):
+    def __init__(self, project_id = 'ID ПРОЕКТА В BQ', creds_folder = CREDS_FOLDER, creds_file = CREDS_FILE, forecast_marker = None, sender = None):
         try:
             from google.oauth2 import service_account
         except:
@@ -19,7 +19,6 @@ class workWithBQ(object):
         self.project_id = project_id
         self.forecast_marker = forecast_marker
         self.sender = sender
-        self.comment_text = comment_text
 
     # Функция чтения из BQ
     def readBySQL(self, sql, need_to_check = True):
@@ -45,8 +44,6 @@ class getForecast(object):
                  sender = None, 
                  forecasting_period = 365, 
                  use_hypo = True, 
-                 sub_forecasts_as_regressor = '', 
-                 get_lower_and_upper = False, 
                  interval_width = 0.8,
                  error_output = False):
         import hashlib
@@ -63,11 +60,8 @@ class getForecast(object):
         self.sql_from_hypo = False # Указывает что в качестве sql запроса был взят запрос из таблицы гиперпараметров
         self.forecasting_period = forecasting_period
         self.use_hypo = use_hypo
-        self.sub_forecasts_as_regressor = sub_forecasts_as_regressor.replace(' ','').split(',') 
         self.session_id = hashlib.md5(self.forecast_unic_id.encode()).hexdigest()
-        self.get_lower_and_upper = get_lower_and_upper
         self.interval_width = interval_width
-        self.comment_text = None
         self.error_output = error_output
         
         # Получаем гиперпараметры и регрессоры, если указана задача прогнозирования
@@ -84,8 +78,7 @@ class getForecast(object):
 
         else:
             # Если надо получить параметры для конкретного набора данных
-            self.parametrs_in_model, self.regressors_in_model, self.found_hypo_records, sql_in_hypo, sub_forecasts, self.comment_text = self.getParametrsAndRegressors(forecastMarker)
-            
+            self.parametrs_in_model, self.regressors_in_model, self.found_hypo_records, sql_in_hypo = self.getParametrsAndRegressors(forecastMarker)
             try:
                 self.parametrs_in_model.update({"interval_width" : interval_width})
             except:
@@ -97,14 +90,6 @@ class getForecast(object):
                 if ((sql_query is None) & (sql_in_hypo is not None)): # Если sql запрос не передан, но такой запрос есть в таблице гиперпараметров
                     self.sql_query = sql_query = sql_in_hypo # Используем sql запрос
                     self.sql_from_hypo = True
-                                
-                if ( 
-                    (len(self.sub_forecasts_as_regressor) == 1)
-                    & 
-                    (self.sub_forecasts_as_regressor[0] == '')
-                   ): # Если запрос на подпрогноз не пришёл в запросе, но указан таблице с гиперпараметрами
-                    self.sub_forecasts_as_regressor = sub_forecasts
-                
             else: 
                 self.isSpecialParametrs = False        
         
@@ -114,8 +99,7 @@ class getForecast(object):
             self.using_sql_query = True
             bq_obj = workWithBQ(
                 forecast_marker = self.forecastMarker, 
-                sender = self.sender, 
-                comment_text = self.comment_text)
+                sender = self.sender)
             
             df = bq_obj.readBySQL(sql_query) 
             self.sended_sql = bq_obj.sended_sql
@@ -130,26 +114,22 @@ class getForecast(object):
         # DOW и регрессоры
         self.dow, self.available_regressors = self.getDOW() # Получаем датафрейм с рабочими и нерабочими днями недели
         
-        # Если использованны значения прогноза, как регрессоры, добавляем их в список регрессоров
-        if self.sub_forecasts_as_regressor[0] != '':
-            self.regressors_in_model += self.sub_forecasts_as_regressor
-        
         # Формируем массив для кросс-валидации полученой модели
         self.fake_yesterday, self.df, self.cutoffs = self.getCutoffs(fake_yesterday)
                 
         # Получаем прогноз при прогнозировании
         if type(self.df) == type(None): # Если пришёл пустой датафрейм
-            self.forecast, self.error_rate, self.m = pd.DataFrame({'ds':['empty_sql','empty_dataframe']}, index = None), -3.14, None
+            self.forecast, self.error_rate, self.m = pd.DataFrame({'ds':['empty_sql','empty_dataframe']}, index=None), -3.14, None
             return None
         
-        self.forecast, self.error_rate, self.m = self.get_forecast(self.df, 'mape', param = self.parametrs_in_model, regressors = self.regressors_in_model)
+        self.forecast, self.error_rate, self.m = self.get_forecast(self.df, 'mape', param=self.parametrs_in_model, regressors = self.regressors_in_model)
         
         return None
            
 
     def getCutoffs(self, fake_yesterday):
         self_df = self.df
-        if type(self_df)!=type(None):
+        if type(self_df) != type(None):
             self_df['ds'] = self_df['ds'].astype('datetime64[ns]')
         
         if fake_yesterday is None: # Если параметр фейкового вчера не задан
@@ -165,18 +145,17 @@ class getForecast(object):
         if type(self_df)!=type(None):
             if (self_df.ds.max() - timedelta(days=30)).date() <= dt.date():
                 days = ((self_df.ds.max() - timedelta(days=30)).date() - dt.date()).days
-                dt = dt.date()+timedelta(days=days)
+                dt = dt.date() + timedelta(days=days)
         
         self_cutoffs = self.pd.to_datetime([str(dt)[:4] + '-' + str(dt)[5:7] + '-' + str(dt)[8:10]])
         return fake_yesterday, self_df, self_cutoffs
 
     
-    # Получаем гиперпараметры и регрессоры, которые относятся с конкретной задаче
+    # Получаем гиперпараметры и регрессоры
     def getParametrsAndRegressors(self, forecastMarker):
         bq_obj = workWithBQ(
             forecast_marker = 'hyperparameters', 
-            sender = self.sender,
-            comment_text = 'Обращаемся к таблице гиперпараметров'
+            sender = self.sender
         ) # Получаем экземпляр класса работы в BQ
         
         # Обращаемся к таблице, хранящей гиперпараметры
@@ -192,8 +171,6 @@ class getForecast(object):
                 param = {} #по умолчанию задаём пустой sql-запрос для прогноза
                 sql_in_hypo = None
                 regressors = ['dow'] # Базовая настройка регрессоров
-                sub_forecasts = ['']
-                comment_text = None
                 
                 if self.use_hypo == True: # Если не стоит запрет на использование гиперпараметров
                     if str(df_hyper.loc[0,'changepoint_prior_scale']) != 'nan':
@@ -208,25 +185,19 @@ class getForecast(object):
                         regressors = df_hyper.loc[0,'regressors'].replace(' ','').split(',')
                     else:
                         regressors = ['dow']
-                    if str(df_hyper.loc[0,'sub_forecasts']) != 'None':
-                        sub_forecasts = df_hyper.loc[0,'sub_forecasts'].replace(' ','').split(',')
-                    else:
-                        sub_forecasts = ['']
 
                 if str(df_hyper.loc[0,'sql']) != 'None':
                     sql_in_hypo = df_hyper.loc[0,'sql'] 
-
-                if str(df_hyper.loc[0,'comment_text']) != 'None':
-                    comment_text = df_hyper.loc[0,'comment_text'] 
-                return param, regressors, df_hyper.shape[0], sql_in_hypo, sub_forecasts, comment_text
+                return param, regressors, df_hyper.shape[0], sql_in_hypo
         else:
-            return None, ['dow'], 0, None, None, None
-        
-    def getDOW(self): # Получаем список доступных регрессоров
+            return None, ['dow'], 0, None
+
+    
+    # Получаем список доступных регрессоров
+    def getDOW(self): 
         bq_obj = workWithBQ(
             forecast_marker = 'DOW', 
-            sender = self.sender,
-            comment_text = 'Получаем набор регрессоров'
+            sender = self.sender
         ) # Получаем экземпляр класса работы в BQ
         
         # Обращаемся к таблице, в которой хранятся доступные значения регрессоров
@@ -238,47 +209,6 @@ class getForecast(object):
         
         df_regressors['ds'] = df_regressors['ds'].apply(lambda x: str(x)[:10])
         df_regressors['ds'] = df_regressors['ds'].astype('datetime64[ns]')       
-
-        # Выполняем задачи прогнозирования, если они переданы как регрессоры
-        def getForecastMarkerAsRegressor(self, sub_forecasts_as_regressor, available_regressors):
-            import json
-            import requests
-            
-            if ( # Если передан пустой массив
-                (len(sub_forecasts_as_regressor) == 1) &
-                (sub_forecasts_as_regressor[0] == '')
-            ):
-                return available_regressors
-            
-            for sub_ft in sub_forecasts_as_regressor:
-                print('---' + sub_ft)
-                print('sub_ft: ' + str(sub_ft))
-                                
-                data = {'forecastMarker' : sub_ft, 'sender' : "internal_call for " + str(self.forecastMarker) + ', session_id: '+str(self.session_id)} # data to be sent to api
-                r = requests.post(url = ENDPOINT_FORECAST, data = data)
-                j = json.loads(r.text) # sending post request and saving response as response object
-
-                result = pd.DataFrame({ # Формируем датафрейм с прогнозом 
-                    'ds':j['ds'], sub_ft: j['iliPlanIliFact']
-                }) 
-
-                result['ds'] = pd.to_datetime(result['ds'], unit='ms') # Переводим время в микросекундах в формат даты
-
-                available_regressors = available_regressors.join(result.set_index('ds'), on = 'ds') # Подклеиваем полученный результат к набору уже добавленных регрессоров
-
-                # Получаем диапазон заполненных данных для нового регрессора и значения для заполнения (среднее 10 значений)
-                last_month = available_regressors[~available_regressors[sub_ft].isna()].iloc[-1]['ds'] # последнее зафиксированное значение
-                first_month = available_regressors[~available_regressors[sub_ft].isna()].iloc[0]['ds'] # первое зафиксированное значение
-
-                last_value = available_regressors[~available_regressors[sub_ft].isna()].iloc[-10:-1][sub_ft].mean() # последнее зафиксированное значение
-                first_value = available_regressors[~available_regressors[sub_ft].isna()].iloc[0:10][sub_ft].mean() # первое зафиксированное значение
-
-                # Заполняем пропущенные данные значениями, которые в начале и в концесуществующего диапазона 
-                available_regressors.loc[available_regressors[available_regressors['ds'] > last_month].index, sub_ft] = last_value # заменям на первое значимое значение всё что было до него
-                available_regressors.loc[available_regressors[available_regressors['ds'] < first_month].index, sub_ft] = first_value # заменям на последнее значимое значение всё что было после
-            return available_regressors      
-        
-        df_regressors = getForecastMarkerAsRegressor(self,self.sub_forecasts_as_regressor, df_regressors)
         return df_regressors[['ds', 'dow']], df_regressors
 
     
@@ -322,12 +252,10 @@ class getForecast(object):
         forecast.loc[i,'iliPlanIliFact']=forecast.loc[i,'yhat']
         
         error_rate = self.get_error_for_model(m, error_type) # Получаем значние ошибки прогноза
-        output_mas = ['ds','y','yhat','iliPlanIliFact','yhat_upper', 'yhat_lower'] # базовый набор возвращаемых значений
-        
-        if self.get_lower_and_upper:
-            output_mas += ['yhat_upper', 'yhat_lower']
+        output_mas = ['ds','y','yhat','iliPlanIliFact'] # базовый набор возвращаемых значений
         
         return  forecast[output_mas], error_rate, m #возвращаем 2 столбца из датафрейма с прогнозом
+
 
     # Функция расчета ошибки прогнозирования
     def get_error_for_model(self, m, error_type):
